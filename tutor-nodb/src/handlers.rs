6f2d0bd926c256415213b9cb173ea801dc6b6dc2
Kode <olorik362@gmail.com>
2025-08-15 21:28:53 +0100

    feat(tutor-nodb): a nodb rest api

---
+++ tutor-nodb/src/handlers.rs
@@ -0,0 +1,16 @@
+use actix_web::{web,Responder,HttpResponse};
+use super::state::AppState;
+
+pub fn general_routes( cfg:&mut web::ServiceConfig){
+	cfg.route("/health",web::get().to(health_check_handler));
+}
+
+async fn health_check_handler(app_state:web::Data<AppState>)->impl Responder{
+let health_check_response  =&app_state.health_check_response;
+let mut visit_count = app_state.visit_count.lock().unwrap();
+let response = format!("{} {} times",health_check_response,visit_count );
+*visit_count +=1;
+drop(visit_count);
+HttpResponse::Ok().json(response)
+
+}
---
+++ tutor-nodb/src/models.rs
@@ -0,0 +1,17 @@
+use actix_web::web;
+use chrono::NaiveDateTime;
+use serde::{Deserialize,Serialize};
+
+#[derive(Debug,Serialize,Deserialize,Clone)]
+pub struct Course{
+	pub tutor_id:i32,
+	pub course_id:Option<i32>,
+	pub course_name:String,
+	pub posted_time:Option<NaiveDateTime>,
+}
+
+impl From<web::Json<Course>> for Course {
+	unimplemented!();
+} {
+	// add code here
+}
